199eefe56bd4996bd05afc784de0708a044146a4
janelle mellor <janellemellor@gmail.com>
2020-05-06 15:50:23 -0700

    add test for characters

---
+++ src/components/Characters/Characters.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Characters from './Characters';
+
+describe('Characters', () => {
+  it('matches a Characters snapshot', () => {
+    const characters = [
+      {
+        id: '5cf5679a915ecad153ab68d3',
+        image: 'https://vignette.wikia.nocookie.net/avatar/images/6/65/Appa_flying.png/revision/latest?cb=20140517110636',
+        name: 'Appa',
+        affiliation: 'Air Nomads Team Avatar'
+      },
+      {
+        id: '5cf5679a915ecad153ab68d6',
+        image: 'https://vignette.wikia.nocookie.net/avatar/images/0/0c/Chief.png/revision/latest?cb=20140122221730',
+        name: 'Arnook',
+        affiliation: 'Northern Water Tribe'
+      }
+    ];
+    const wrapper = shallow(<Characters characters={characters} />);
+    expect(wrapper).toMatchSnapshot();    
+  });
+});
+
+
