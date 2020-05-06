57771734f8b13e058d8a8027711372328c852cc7
janelle mellor <janellemellor@gmail.com>
2020-05-06 13:43:39 -0700

    add mock for api fetch

---
+++ src/services/__mocks__/fileMock.js
@@ -0,0 +1,9 @@
+export const fetchCharacters = () => {
+  return Promise.resolve({
+    id: '5cf5679a915ecad153ab68d6',
+    name: 'Arnook',
+    affiliation: 'Northern Water Tribe',
+    image: 'https://vignette.wikia.nocookie.net/avatar/images/0/0c/Chief.png/revision/latest?cb=20140122221730',
+  });
+};
+
